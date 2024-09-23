7faebca159ffafd112e3d32a3b5f72f1c59f970a
shadow <krushna.gutte78@gmail.com>
2024-09-24 01:58:12 +0530

    added header
    just the introduction

---
+++ src/app/HomePage/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex-1">LOGO</div>
+      <div className="flex flex-1 justify-center">
+        <div>Middle1</div>
+        <div>Middle2</div>
+        <div>Middle3</div>
+      </div>
+      <div className="flex-1 flex justify-end">Help</div>
+    </div>
+  );
+}
--- src/app/HomePage/HomePage.tsx
+++ src/app/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import Background from "./Background";
-import FooTer from "./FooTer";
+import FooTer from "./Header";
 function HomePage() {
   return (
     <>
