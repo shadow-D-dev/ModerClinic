3e53d0dfa925e73ff3e8d0e96194daecbaedd0f6
shadow <krushna.gutte78@gmail.com>
2024-09-25 19:16:48 +0530

    ignore

--- src/app/HomePage/ProfilePage/LeftSection.tsx
+++ src/app/HomePage/ProfilePage/LeftSection.tsx
@@ -1,7 +1,7 @@
 export default function LeftSection() {
   return (
     <div className="flex w-screen border border-white rounded-lg justify-center p-20">
-      LeftSection
+      <h1>Hello</h1>
     </div>
   );
 }
