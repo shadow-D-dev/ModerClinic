f796896ca85f109685747dab08fd4f50b7257d84
shadow <krushna.gutte78@gmail.com>
2024-10-08 00:19:34 +0530

    fixed the absoulte issue for the image
    fixed the position of the main image on the homepage and the floating
    div over it

--- src/app/HomePage/ProfilePage/ProfilePage.tsx
+++ src/app/HomePage/ProfilePage/ProfilePage.tsx
@@ -2,7 +2,7 @@ import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 export default function ProfilePage() {
   return (
-    <div className=" flex text-5xl gap-4 w-full">
+    <div className=" flex text-5xl gap-4 w-full h-[1000px]">
       <div className="w-1/2">
         <LeftSection />
       </div>
--- src/app/HomePage/ProfilePage/RightSection.tsx
+++ src/app/HomePage/ProfilePage/RightSection.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import image from "@/../Public/images/background3.jpg";
+import FloatingDiv from "./Floating/FloatingDiv";
 export default function RightSection() {
   return (
-    <div className="w-full">
-      <div className="flex  p-10 ">
-        <Image
-          src={image}
-          alt="treatment image"
-          className="rounded-3xl h-[650px] w-[570px]"
-        />
-      </div>
-      <div className=" top-96">Best services</div>
+    <div className="relative">
+      <Image
+        src={image}
+        alt="treatment image"
+        className="rounded-3xl h-[650px] w-[570px] absolute right-8 "
+      />
+      <FloatingDiv />
     </div>
   );
 }
