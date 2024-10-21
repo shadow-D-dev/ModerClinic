b5c47c61a174f282f3fc23973e5057ab16f0ba56
shadow <krushna.gutte78@gmail.com>
2024-10-21 20:16:16 +0530

    ------Moving to mobile responsiveness----------

--- src/app/HomePage/Background.tsx
+++ src/app/HomePage/Background.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import background from "@/../Public/Images/hero-bg.svg";
 export default function Background() {
   return (
-    <div className="relative w-screen">
+    <div className="relative ">
       <div className=" opacity-60  bg-gradient-to-r from-black to-black">
         <Image
           src={background}
           alt="background"
-          className="bg-[#5e5eee]  bg-gradient-to-r from-[#5e5eee] to-black absolute w-screen"
+          className="bg-[#5e5eee]  bg-gradient-to-r from-[#5e5eee] to-black absolute"
         />
       </div>
     </div>
--- src/app/HomePage/ProfilePage/ProfilePage.tsx
+++ src/app/HomePage/ProfilePage/ProfilePage.tsx
@@ -2,7 +2,7 @@ import LeftSection from "./LeftSection";
 import RightSection from "./RightSection";
 export default function ProfilePage() {
   return (
-    <div className="flex w-[1500px] ">
+    <div className="flex w-[1500px] md:flex-col md:w-[1000px]">
       <div className="w-1/2 mt-32">
         <LeftSection />
       </div>
